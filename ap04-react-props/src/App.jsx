ac70993a9416d02ed35c2e1edd51c699e6a0f5d8
Kauan Torres <darqueboost@gmail.com>
2026-03-13 19:53:17 -0300

    learn(ap04): adiciona botões

--- ap04-react-props/src/App.jsx
+++ ap04-react-props/src/App.jsx
@@ -1,7 +1,19 @@
 import Pedido from "./Pedido";
 import Cartao from "./Cartao";
+import Feedback from "./Feedback";
 
 const App = () => {
+  const textoOK = 'Já chegou'
+  const textoNOOK = 'Não chegou'
+  const funcaoOK = () => alert('Obrigado pelo feedback')
+  const funcaoNOOK = () => alert('Verificaremos')
+  const componenteFeedback = (
+    <Feedback
+      textoOK = {textoOK}
+      textoNOOK = {textoNOOK}
+      funcaoOK = {funcaoOK}
+      funcaoNOOK = {funcaoNOOK}/>)
+  
   return (
     <div className="container-fluid">
       <div className="row">
@@ -12,7 +24,11 @@ const App = () => {
       <div className="row">
         <div className="col-sm-12 col-md-6 col-xl-3">
           <Cartao cabecalho="22/04/2024">
-            <Pedido icone="book" titulo="Livro" desc="Um bom livro" />
+            <Pedido 
+              icone="book" 
+              titulo="Livro" 
+              desc="Um bom livro" />
+              {componenteFeedback}
           </Cartao>
         </div>
         <div className="col-sm-12 col-md-6 col-xl-3">
@@ -21,8 +37,8 @@ const App = () => {
               data="22/04/2024"
               icone="camera"
               titulo="Camera"
-              desc="Camera 4K"
-            />
+              desc="Camera 4K"/>
+              {componenteFeedback}
           </Cartao>
         </div>
         <div className="col-sm-12 col-md-6 col-xl-3">
@@ -31,8 +47,8 @@ const App = () => {
               data="22/04/2024"
               icone="bicycle"
               titulo="Bicicleta"
-              desc="Bela bicicleta de 2 rodas"
-            />
+              desc="Bela bicicleta de 2 rodas"/>
+              {componenteFeedback}
           </Cartao>
         </div>
         <div className="col-sm-12 col-md-6 col-xl-3">
@@ -41,8 +57,8 @@ const App = () => {
               data="15/12/2025"
               icone="hippo"
               titulo="Hipopotamo"
-              desc="Lindo hipopotamo"
-            />
+              desc="Lindo hipopotamo"/>
+              {componenteFeedback}
           </Cartao>
         </div>
       </div>
--- ap04-react-props/src/Feedback.jsx
+++ ap04-react-props/src/Feedback.jsx
@@ -2,12 +2,16 @@ const Feedback = (props) => {
   return (
     <div className="d-flex justify-content-evenly m-3">
         <button
+        onClick={props.funcaoOK}
         type="button"
         className="btn btn-primary">
+          {props.textoOK}
         </button>
         <button
+         onClick={props.funcaoNOOK}
         type="button"
         className="btn btn-primary">
+          {props.textoNOOK}
         </button>
     </div>
   )
